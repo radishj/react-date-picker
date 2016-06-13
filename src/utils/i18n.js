66f934c149e0dfa9d50ce1de4c70cd262a739dcf
baili(百里) <423261989@qq.com>
2016-06-13 11:48:37 +0800

    add zh-tw

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -4,13 +4,22 @@ const I18N = {
     clearButtonText: 'Clear',
     okButtonText: 'OK',
     cancelButtonText: 'Cancel',
+    default: 'None'
   },
   'zh-cn': {
     todayButtonText: '今天',
     clearButtonText: '清除',
     okButtonText: '确定',
     cancelButtonText: '取消',
-  }
+    default: 'None'
+  },
+  'zh-tw': {
+    todayButtonText: '今天',
+    clearButtonText: '清除',
+    okButtonText: '確定',
+    cancelButtonText: '取消',
+    default: 'None'
+  },
 }
 
 export default function i18n(locale, key){
@@ -18,7 +27,8 @@ export default function i18n(locale, key){
   locale = locale && I18N[locale] ? locale : 'en'
 
   // default value
-  var value = key
+  var value = I18N[locale].default
+
   // translate value
   if(I18N[locale][key]) {
     value = I18N[locale][key]
